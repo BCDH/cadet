2fc464dbfdbc1a004fb150e1eb18c87320749b80
ajanco <apjanco@gmail.com>
2020-12-21 12:40:20 -0500

    files uploading

--- app/main.py
+++ app/main.py
@@ -1,24 +1,37 @@
 from datetime import datetime
 from typing import Optional
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form, Depends,HTTPException,status
 from fastapi.templating import Jinja2Templates
-
+import secrets
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+from app.routers import texts
+
 from pathlib import Path
 app_dir = Path.cwd()
 templates = Jinja2Templates(directory="app/templates")
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='./app/static'), name='static')
+app.include_router(texts.router)
 
-PASSWORD = 'fishandchips'
+security = HTTPBasic()
 
-
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "stanleyjobson")
+    correct_password = secrets.compare_digest(credentials.password, "swordfish")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 #app.include_router(home.router)
 #app.include_router(weather_app.router)
 
 #routers
-# social authentication 
 # load/connect to corpus, get seeds for annotation, create initial ling data
 # create language object
 # create config.cfg and train
@@ -34,16 +47,9 @@ PASSWORD = 'fishandchips'
 @app.get("/")
 def root(request: Request, ):
    
-    return templates.TemplateResponse("index.html", {"request": request, "time": datetime.now() })
+    return templates.TemplateResponse("login.html", {"request": request })
 
-from fastapi import FastAPI, File, Form, UploadFile
+@app.get("/main")
+def main(request: Request, username: str = Depends(get_current_username)):
+        return templates.TemplateResponse("main_page.html", {"request": request })
 
-@app.post("/")
-def login(request: Request, password: str = Form(...)):
-        
-    if password == PASSWORD: 
-        
-        return templates.TemplateResponse("index.html", {"request": request, })
-    else:
-        message = "Please try again"
-        return templates.TemplateResponse("index.html", {"request": request, "message": message })
