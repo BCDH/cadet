975f21ddd7782c9884d9b44287b3659f27d1834e
ajanco <apjanco@gmail.com>
2020-12-26 16:42:13 -0500

    adding galazy pics

--- app/test_all.py
+++ app/test_all.py
@@ -8,9 +8,16 @@ client = TestClient(app)
 def test_read_open():
     response = client.get("/")
     assert response.status_code == 200
-    #assert response.json() == {"msg": "Hello World"}
+    
 
 def test_read_main_no_password():
     response = client.get("/main")
     assert response.status_code == 401
   
+def test_create_language():
+    pass
+    #assert that works with various problems
+
+def test_clone_language():
+    pass
+    #assert that works for all existing languages and their dependencies
