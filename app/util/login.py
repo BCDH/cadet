dba710ca236703d51a85a001d1ef9b6c45355e89
ajanco <apjanco@gmail.com>
2020-12-29 21:43:40 -0500

    switch to env password with defaults

--- app/util/login.py
+++ app/util/login.py
@@ -1,12 +1,21 @@
+import os
 import secrets
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 security = HTTPBasic()
+try:
+    env_username = os.environ['CADET_PASSWORD']
+except KeyError:
+    os.environ['CADET_PASSWORD'] = 'cadet'
+try:
+    env_password = os.environ['CADET_PASSWORD']
+except  KeyError:
+    os.environ['CADET_PASSWORD'] = 'NewNLP'
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "cadet")
-    correct_password = secrets.compare_digest(credentials.password, "NewNLP")
+    correct_username = secrets.compare_digest(credentials.username, env_username)
+    correct_password = secrets.compare_digest(credentials.password, env_password)
     if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
