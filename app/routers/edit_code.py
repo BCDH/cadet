41ac7a64e18efff179690625c752521f76f6a081
ajanco <apjanco@gmail.com>
2021-01-01 08:46:20 -0500

    tokenization now loading lang obj

--- app/routers/edit_code.py
+++ app/routers/edit_code.py
@@ -2,9 +2,10 @@ import srsly
 from pathlib import Path
 from typing import List, Optional
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi import Request, Form, File, UploadFile
 from fastapi.templating import Jinja2Templates
+from app.util.login import get_current_username
 
 templates = Jinja2Templates(directory="app/templates")
 
@@ -12,7 +13,20 @@ router = APIRouter()
 
 
 @router.get("/edit")
-async def read_items(request: Request, file_name:str):
+async def read_items(request: Request, file_name:str, username: str = Depends(get_current_username)):
+    code = f'hi there! {file_name}'
+    new_lang = (Path.cwd() / 'new_lang')
+    if len(list(new_lang.iterdir())) > 0:
+        path = list(new_lang.iterdir())[0]
+        path = path / file_name
+        if path.exists():
+            code = path.read_text()
+        else:
+            code = 'Error, please enter a valid filename'
+    return templates.TemplateResponse("edit_code.html", {"request": request, "code":code})
+
+@router.post("/edit")
+async def read_items(request: Request, file_name:str, username: str = Depends(get_current_username)):
     code = f'hi there! {file_name}'
     new_lang = (Path.cwd() / 'new_lang')
     if len(list(new_lang.iterdir())) > 0:
--- app/routers/tokenization.py
+++ app/routers/tokenization.py
@@ -1,14 +1,35 @@
+import spacy
 from pathlib import Path
 from fastapi import APIRouter
 from fastapi import Request, Form, Depends
 from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="app/templates")
 from app.util.login import get_current_username
+import importlib.util # https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
 
 router = APIRouter()
 
 
 @router.get("/tokenization")
 async def tokenization(request: Request, login = Depends(get_current_username)):
-    print(login)
-    return templates.TemplateResponse("tokenization.html", {"request": request, })
+    new_lang = (Path.cwd() / 'new_lang')
+    if len(list(new_lang.iterdir())) > 0:
+        path = list(new_lang.iterdir())[0]
+        path = path / 'examples.py'
+        spec = importlib.util.spec_from_file_location("sentences", str(path))
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        sentences = module.sentences
+
+        # Load language object as nlp
+        lang_name = list(new_lang.iterdir())[0].name
+        mod = __import__(f'new_lang.{lang_name}', fromlist=[lang_name.capitalize()])
+        cls = getattr(mod, lang_name.capitalize())
+        nlp = cls()
+
+        for sentence in sentences:
+            doc = nlp(sentence)
+            for token in doc:
+                print(token)
+
+    return templates.TemplateResponse("tokenization.html", {"request": request, "sentences":sentences })
