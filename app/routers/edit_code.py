05b1ee2c8d663c318220824f7a92cbdeaa7a5d63
ajanco <apjanco@gmail.com>
2021-01-06 08:39:10 -0500

    code editor now saving/updating to disk

--- app/routers/edit_code.py
+++ app/routers/edit_code.py
@@ -2,7 +2,7 @@ import srsly
 from pathlib import Path
 from typing import List, Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi import Request, Form, File, UploadFile
 from fastapi.templating import Jinja2Templates
 from app.util.login import get_current_username
@@ -13,8 +13,7 @@ router = APIRouter()
 
 
 @router.get("/edit")
-async def read_items(request: Request, file_name:str, username: str = Depends(get_current_username)):
-    code = f'hi there! {file_name}'
+async def edit_code(request: Request, file_name:str, username: str = Depends(get_current_username)):
     new_lang = (Path.cwd() / 'new_lang')
     if len(list(new_lang.iterdir())) > 0:
         path = list(new_lang.iterdir())[0]
@@ -22,18 +21,24 @@ async def read_items(request: Request, file_name:str, username: str = Depends(ge
         if path.exists():
             code = path.read_text()
         else:
-            code = 'Error, please enter a valid filename'
+            raise HTTPException(status_code=404, detail="File not found")
     return templates.TemplateResponse("edit_code.html", {"request": request, "code":code})
 
 @router.post("/edit")
-async def read_items(request: Request, file_name:str, username: str = Depends(get_current_username)):
-    code = f'hi there! {file_name}'
+async def update_code(
+    request: Request, 
+    username: str = Depends(get_current_username)):
+    
+    data = await request.json()
+    filename = data['filename']
+    code = data['code']
+
     new_lang = (Path.cwd() / 'new_lang')
     if len(list(new_lang.iterdir())) > 0:
         path = list(new_lang.iterdir())[0]
-        path = path / file_name
+        path = path / filename
         if path.exists():
-            code = path.read_text()
+            path.write_text(code)
         else:
-            code = 'Error, please enter a valid filename'
+            raise HTTPException(status_code=404, detail="File not found")
     return templates.TemplateResponse("edit_code.html", {"request": request, "code":code})
