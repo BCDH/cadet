c8a668a2199e79da42d986b4cf347991e2308ac8
ajanco <apjanco@gmail.com>
2021-04-18 14:01:48 -0400

    csv and json save working

--- app/routers/lookups.py
+++ app/routers/lookups.py
@@ -1,5 +1,6 @@
 import httpx
 import json
+import csv
 import srsly
 from pathlib import Path
 from typing import List, Optional
@@ -21,10 +22,37 @@ async def read_items(request: Request):
 @router.post("/upload_lookups")
 async def update_lookups(file: UploadFile = File(...),lookup_type:str= Form(...)):
     contents = file.file.read()
-    if file.content_type == 'application/pdf':
-        print('pdf',lookup_type, len(contents))
-    else:
-        print(lookup_type, len(contents))
+    contents = contents.decode("utf-8")
+
+    #load lookups file 
+    new_lang = Path.cwd() / "new_lang"
+    if len(list(new_lang.iterdir())) > 0:
+        path = list(new_lang.iterdir())[0] / "lookups"
+        if lookup_type == "pos":
+            json_file = list(path.glob("*upos*"))[0]
+        if lookup_type == "lemma":
+            json_file = list(path.glob("*lemma*"))[0]
+        if lookup_type == "ent":
+            json_file = list(path.glob("*entity*"))[0]
+        if json_file.exists():
+            lookup = srsly.read_json(json_file)
+
+    # load CSV file 
+    if file.content_type == 'text/csv':
+        reader = csv.reader(contents.splitlines())
+        for row in reader:
+            if row[0] == 'key' and row[1] == 'value':
+                pass
+            else:
+                lookup[row[0]] = row[1] 
+        srsly.write_json(json_file, lookup)
+            
+        
+    if file.content_type == 'application/json':
+        data = srsly.json_loads(contents)
+        join_dicts = {**lookup, **data}
+        srsly.write_json(json_file, join_dicts)
+    
 
 
 ###None of these are needed vvv
