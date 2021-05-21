1a3eb0468df52698c6e0804f61453eb68eb467fa
ajanco <apjanco@gmail.com>
2021-05-20 21:12:58 -0400

    working on project.py

--- app/routers/project.py
+++ app/routers/project.py
@@ -32,88 +32,52 @@ async def project(request:Request):
 async def download_project():
     """Package all files noted in the template. 
     1. original text files.
-    2.  template datasheet for your dataset.
-    3.  lookups for lemma, pos and entities
-    4. serialize your new spaCy language object
-    5. a spaCy project file
+    2.  lookups for lemma, pos and entities
+    3. serialize your new spaCy language object
+    4. a spaCy project file
 
     Returns:
         FileResponse: project zip file
     """
     new_lang = Path.cwd() / "new_lang"
     lang_name = list(new_lang.iterdir())[0].name
-    lookups_path = new_lang / lang_name / "lookups"
-    for lookup in lookups_path.iterdir():
-        key = lookup.stem[lookup.stem.find('_') + 1:]
-        if 'lemma' in key:
-            lemma_data = srsly.read_json(lookup)
-            if isinstance(lemma_data, str):
-                return RedirectResponse("/edit_lookup?type=lemma")
-
-        if 'entity' in key:
-            entity_data = srsly.read_json(lookup)
-            if isinstance(entity_data, str):
-                return RedirectResponse("/edit_lookup?type=entity")
-        if 'pos' in key:
-            pos_data = srsly.read_json(lookup)
-            if isinstance(pos_data, str):
-                return RedirectResponse("/edit_lookup?type=pos")
     
-    #if valid, continue
-    texts = get_texts()
-    filenames = get_filenames()
+    # 1 Texts
+    texts_path = list(new_lang.iterdir())[0] / "texts"
+    texts = [text for text in texts_path.iterdir()]
+    
+    # 2 lookups 
+    lookups_path = new_lang / lang_name / "lookups"
+    lookups = [l for l in lookups_path.iterdir()]
+            
+    # 3 NLP object
     nlp = get_nlp()
-    if texts and nlp: 
-        docs = [doc for doc in list(nlp.pipe(texts))]
-        
-        # let each Doc remember the file it came from
-        for doc, filename in zip(docs,filenames):
-            doc.user_data['filename'] =  filename
+    #TODO serialize to_disk() or save folder? does to_disk save examples, exceptions?
 
-        docs = update_tokens_with_lookups(nlp, docs)
-        conll = [doc_to_conll(doc) for doc in docs]
+    # 4 project.yml
 
-        temp_path = Path('/tmp/project_export')
-        temp_path.mkdir(parents=True, exist_ok=True)
-        for filename, conll in zip(filenames,conll):
-            conll_filename = filename.split('.')[0] +'.conll'
-            (temp_path / conll_filename).write_text(conll)
+    temp_path = Path('/tmp/project_export')
+    temp_path.mkdir(parents=True, exist_ok=True)
+    
+    # TODO copy files to temp_path
 
-        #shutil.make_archive("zipped_sample_directory", "zip", "sample_directory")
-        shutil.make_archive(str(temp_path), 'zip', str(temp_path))
-        zip_file = str(temp_path).split('/')[-1]+'.zip'
-        #save each doc to a file, return single zip file with all CONFIRM, can import directory into INCEpTION
 
-        return FileResponse(f'/tmp/project_export.zip', media_type="application/zip", filename=lang_name + '_'+ zip_file)
+    #shutil.make_archive("zipped_sample_directory", "zip", "sample_directory")
+    shutil.make_archive(str(temp_path), 'zip', str(temp_path))
+    zip_file = str(temp_path).split('/')[-1]+'.zip'
+    #save each doc to a file, return single zip file with all CONFIRM, can import directory into INCEpTION
 
-def get_filenames() -> List[str]:
-    new_lang = Path.cwd() / "new_lang"
-    if len(list(new_lang.iterdir())) > 0:
-        texts_path = list(new_lang.iterdir())[0] / "texts"
-        if not texts_path.exists():
-            return None
-        filenames = [text.name for text in texts_path.iterdir()]
-        return filenames
+    return FileResponse(f'/tmp/project_export.zip', media_type="application/zip", filename=lang_name + '_'+ zip_file)
 
-def get_texts() -> List[str]:
-    new_lang = Path.cwd() / "new_lang"
-    if len(list(new_lang.iterdir())) > 0:
-        texts_path = list(new_lang.iterdir())[0] / "texts"
-        if not texts_path.exists():
-            return None
-        texts = [text.read_text() for text in texts_path.iterdir()]
-        return texts
 
 def get_nlp():
     # Load language object as nlp
     new_lang = Path.cwd() / "new_lang"
     lang_name = list(new_lang.iterdir())[0].name
-    try:
-        mod = __import__(f"new_lang.{lang_name}", fromlist=[lang_name.capitalize()])
-    except SyntaxError:  # Unable to load __init__ due to syntax error
-        # redirect /edit?file_name=examples.py
-        message = "[*] SyntaxError, please correct this file to proceed."
-        return RedirectResponse(url="/edit?file_name=tokenizer_exceptions.py")
+    mod = __import__(f"new_lang.{lang_name}", fromlist=[lang_name.capitalize()])
     cls = getattr(mod, lang_name.capitalize())
     nlp = cls()
     return nlp
+    
+        
+    
